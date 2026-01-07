fc01d48e7223f014912bdeca023c2163973657c4
IlyaSaltanov <mail@saltanov-pro.ru>
2026-01-07 22:04:17 +0500

    написал showInputError и hideInputError

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -9,3 +9,18 @@ const validationSettings = {
   errorClass: "popup__error_visible",
 };
 
+// 1. showInputError — отображает сообщение об ошибке
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(settings.errorClass);
+};
+
+// 2. hideInputError — скрывает сообщение об ошибке
+const hideInputError = (formElement, inputElement, settings) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.textContent = '';
+  errorElement.classList.remove(settings.errorClass);
+};
